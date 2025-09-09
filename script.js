ef2f9839e74fce6ca8c84b6ec10390cdaf29664f
Simon Mwangi <simonmwangi749@gmail.com>
2025-09-09 08:14:47 +0300

    .....

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const smoother = ScrollSmoother.create({
     smooth: 2.5,
     smoothTouch: 0.1,
     effects: true
-}); 
+})
 
 const tl = gsap.timeline();
 
@@ -49,3 +49,58 @@ tl.to(".page1", {
     left: "0%",
     opacity: 1,
 }, 'a')
+
+
+const tl2 = gsap.timeline({
+    ScrollTrigger: {
+        trigger: ".page1",
+        start: "20% 20%",
+        end: "150% 20%",
+        scrub: 1,
+        pin: true
+    }
+});
+
+tl2.to(".nextPage", {
+    transform: "translate(-50%, -50%)"
+}, 'a')
+.from(".nextPage .top h1",{
+    delay: 0.3,
+    top: "33%",
+    left: "25%"
+}, 'a')
+.from("#textContent",{
+    delay: 0.3,
+    top: "120%",
+    left: "-25%",
+}, 'a')
+.from("#left img", {
+    delay: 0.3,
+    width: "10rem",
+    height: "10rem",
+    top: "-10%",
+    rotate: "-22deg",
+    borderRadius: "10px"
+
+}, 'a') 
+.from("#right img", {
+    delay: 0.3,
+    width: "10rem",
+    height: "10rem",
+    top: "15%",
+    left: "25%",
+    rotate: "-22deg",
+    borderRadius: "10px"    
+}, 'a')
+.from("#secondImage, #firstImage", {
+    delay: 0.3,
+    width: "10rem",
+    height: "10rem",
+    top: "50%",
+    borderRadius: "10px"
+}, 'a')
+.set("#left, #right", {
+    overflow: "hidden"
+})
+
+tl2.to({}, {duration: 0.2}, "+=0")
